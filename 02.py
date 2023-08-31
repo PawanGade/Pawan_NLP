4c08809f03517bcdcea89b0483ded403adb7126d
Pawan Gade <gadepawan2002@gmail.com>
2023-08-31 16:49:43 +0530

    Pawan

--- 02.py
+++ 02.py
@@ -1,18 +1,4 @@
-#Tokanization
 import spacy
-nlp = spacy.load("en_core_web_sm")
-about_text = (
-    "Gus Proto is a Python developer currently"
-    " working for a London-based Fintech"
-    " company. He is interested in learning"
-    " Natural Language Processing."
-)
-about_doc = nlp(about_text)
-
-for token in about_doc:
-    print (token, token.idx)
-
-#Stop Word
 spacy_stopwords = spacy.lang.en.stop_words.STOP_WORDS
 len(spacy_stopwords)
 
@@ -27,7 +13,3 @@ for stop_word in list(spacy_stopwords)[:10]:
 nlp = spacy.load("en_core_web_sm")
 about_doc = nlp(custom_about_text)
 print([token for token in about_doc if not token.is_stop])
-
-
-
-
