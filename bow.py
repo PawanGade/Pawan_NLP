ccf1868f3d946e2b0840df56bd82b0803a7d0858
Pawan Gade <100061039+PawanGade@users.noreply.github.com>
2023-09-21 16:22:46 +0530

    Add files via upload

---
+++ bow.py
@@ -0,0 +1,43 @@
+# Name: Pawan Gade
+# Roll No: 20
+######################################
+
+import gensim
+import pprint
+from gensim import corpora
+from gensim.utils import simple_preprocess
+doc_list = [
+   "Hello, how are you?", "How do you do?", 
+   "Hey what are you doing? yes you What are you doing?"
+]
+doc_tokenized = [simple_preprocess(doc) for doc in doc_list]
+dictionary = corpora.Dictionary()
+BoW_corpus = [dictionary.doc2bow(doc, allow_update=True) for doc in doc_tokenized]
+print(BoW_corpus)
+id_words = [[(dictionary[id], count) for id, count in line] for line in BoW_corpus]
+print(id_words)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# OUTPUT
+
+# [[(0, 1), (1, 1), (2, 1), (3, 1)], [(2, 1), (3, 1), (4, 2)], [(0, 2), (3, 3), (5, 2), (6, 1), (7, 2), (8, 1)]]
+# [[('are', 1), ('hello', 1), ('how', 1), ('you', 1)], [('how', 1), ('you', 1), ('do', 2)], [('are', 2), ('you', 3), ('doing', 2), ('hey', 1), ('what', 2), ('yes', 1)]]
